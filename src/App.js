b130cc7b445eb5d4c889dab5edaa8ccf5102a2c7
mosesrc <ryanchristophermoses@gmail.com>
2022-08-29 12:24:38 -0400

    Part 1: React Language

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+const todos = [
+  { body: "Learn React Fundamentals", done: true },
+  { body: "Build a TODOs App", done: false },
+  { body: "Build a Game", done: false },
+];
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <header>TODO List</header>
+
+        <ul>
+          {todos.map((todo) => (
+            <li>{todo.body}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
-
 export default App;
